2e4f5e903f0dc6018afa82e8fb0d3b72c43a8eea
cmorox <morox@LAPTOP-3R0DTD63.localdomain>
2022-12-12 16:26:45 -0600

    Se modifico la carpeta app implementando pandas

--- app/main.py
+++ app/main.py
@@ -1,15 +1,33 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
+  
+  #Código para generar el pie chart sin usar PANDAS
+  '''
   data = read_csv.read_csv('data.csv')
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
+  '''
+  # #Código equivalente usando PANDAS
   
+  # df(dataframe) 
+  df = pd.read_csv("data.csv")  # Nos ahorramos el método creado read_csv.py
+  df = df[df['Continent'] == 'Africa']  
+  # Equivalente a -> data = list(filter(lambda item : item['Continent'] == 'South America',data))
+  countries = df['Country'].values
+  # Equivalente a -> countries = list(map(lambda x: x['Country'], data))
+  percentages = df['World Population Percentage'].values
+  # Equivalente a -> percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, percentages)
+
+
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
   print(country)
 
@@ -19,8 +37,7 @@ def run():
     country = result[0]
     print(country)
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(country["Country"], labels, values)
-  
+    charts.generate_bar_chart(country['Country'], labels, values)
 
 if __name__ == '__main__':
   run()
